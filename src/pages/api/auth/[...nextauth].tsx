9798be9514b415cefe1c4f266f4c9ecefddd1efd
Lucas Felix <lucasfelixdev@gmail.com>
2021-05-04 12:38:19 -0300

    Revert "fix: Atualização dos id de autenticação"
    This reverts commit 3e816536c9e14db72721c33fb100f5b4a1663796.

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -5,10 +5,8 @@ import Providers from 'next-auth/providers'
 const options = {
   providers: [
     Providers.Google({
-      // clientId: process.env.GOOGLE_ID_PRODUCTION,
-      // clientSecret: process.env.GOOGLE_SECRET_PRODUCTION,
-      clientId: '107644619120-j70h9tmt36j6oo6hm4h4gscj13i854ds.apps.googleusercontent.com',
-      clientSecret: '2mtbkmDPuShEO-VLeQ7Qg3mf'
+      clientId: process.env.GOOGLE_ID_PRODUCTION,
+      clientSecret: process.env.GOOGLE_SECRET_PRODUCTION,
     }),
   ]
 }
