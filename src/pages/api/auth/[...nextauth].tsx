3e816536c9e14db72721c33fb100f5b4a1663796
Lucas Felix <lucasfelixdev@gmail.com>
2021-05-04 12:18:06 -0300

    fix: Atualização dos id de autenticação

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -5,8 +5,10 @@ import Providers from 'next-auth/providers'
 const options = {
   providers: [
     Providers.Google({
-      clientId: process.env.GOOGLE_ID_PRODUCTION,
-      clientSecret: process.env.GOOGLE_SECRET_PRODUCTION,
+      // clientId: process.env.GOOGLE_ID_PRODUCTION,
+      // clientSecret: process.env.GOOGLE_SECRET_PRODUCTION,
+      clientId: '107644619120-j70h9tmt36j6oo6hm4h4gscj13i854ds.apps.googleusercontent.com',
+      clientSecret: '2mtbkmDPuShEO-VLeQ7Qg3mf'
     }),
   ]
 }
